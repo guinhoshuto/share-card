57011d235cd6bfaad3a11fcebb0a1dd36e93d47e
Guilherme Shuto <guinho.shuto@gmail.com>
2023-08-16 15:48:16 +0000

    sharing ok

--- src/App.js
+++ src/App.js
@@ -1,34 +1,62 @@
 import { useEffect, useState } from 'react';
+import { DownloadSimple, Share} from "@phosphor-icons/react"
 import './App.css';
 const image = "https://images.pexels.com/photos/15272405/pexels-photo-15272405/free-photo-of-fotografia-aerea-aerofotografia-tiro-com-drone-natureza.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+// const image = '/img.jpeg'
 
 function App() {
   const [canShare, setCanShare] = useState()
 
+  // async function imageBlob(url){
+  //   const blob = await fetch(url).then(r => r.blob())
+  //   return new File([blob], 'img.png', {
+  //     type: blob.type
+  //   })
+  // }
+
   useEffect(() => {
-    console.log(navigator.canShare())
-    if(navigator.canShare()){
+    // console.log(navigator.canShare())
+    // const image = await imageBlob(imageUrl)
+    if(navigator.share){
       setCanShare(true)
     } else {
       setCanShare(false)
     }
-  }, [])
+  }, )
 
-  function handleClick(e){
-    console.log("clicked", e.currentTarget.dataset.img);
+  async function handleClick(e){
+    const mediaUrl = e.currentTarget.dataset.img;
+    console.log("clicked", mediaUrl);
+
+    if(canShare) {
+      const file = await fetch(image).then(r => r.blob())
+
+      await navigator.share({
+        files: [ 
+          new File([file], 'img.jpeg', {
+            type: file.type
+          })
+        ]
+      })
+    }
   }
 
   return (
     <div className="bg-gray-800 h-screen w-full flex items-center justify-center">
-      <div 
-        data-img={image}
-        onClick={(e) => handleClick(e)} 
-        className="group relative w-64 h-96 rounded-xl overflow-hidden cursor-pointer shadow-xl">
-        <img src={image} alt="card image" className='object-cover w-full h-full'/>
-        <div className='absolute top-0 right-0 h-full w-full bg-black opacity-0 group-hover:opacity-40 duration-500 flex items-center justify-center'>
-          <div className='text-white text-2xl'>{canShare ? "Share" : "Not supported"}</div> 
+      <a href={!canShare ? image : 'javascript:void(0)'} download="image" >
+        <div 
+          data-img={image}
+          onClick={(e) => handleClick(e)} 
+          className="group relative w-64 h-96 rounded-xl overflow-hidden cursor-pointer shadow-xl"
+          >
+          <img src={image} alt="card image" className='object-cover w-full h-full'/>
+          <div className='absolute top-0 right-0 h-full w-full bg-black opacity-0 group-hover:opacity-40 duration-500 flex items-center justify-center'>
+            <div className='text-white text-2xl'>
+              {canShare ? (<Share size={32} weight="fill" />) : (<DownloadSimple size={32} weight="fill" />)}
+            </div> 
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 }
